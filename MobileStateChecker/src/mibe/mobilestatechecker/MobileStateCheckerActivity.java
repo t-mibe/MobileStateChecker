552de297d7880117d4e08193337e209e6be4690d
mibe <mibe@horowitz>
2012-04-26 03:31:16 +0900

    トグルボタンの挙動を更新 トースト表示

--- MobileStateChecker/src/mibe/mobilestatechecker/MobileStateCheckerActivity.java
+++ MobileStateChecker/src/mibe/mobilestatechecker/MobileStateCheckerActivity.java
@@ -2,12 +2,50 @@ package mibe.mobilestatechecker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MobileStateCheckerActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	
+	private ToggleButton tb;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		// トグルボタンを押したときの挙動を設定
+		setToggleButton();
+	}
+	
+	// トグルボタンを押したときの挙動を設定
+	private void setToggleButton() {
+		
+		// IDからトグルボタンを取得
+		tb = (ToggleButton)findViewById(R.id.toggleButton);
+		
+		// トグルボタンに変更があった時の挙動を設定
+		tb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			// 変更があった時の挙動
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				
+				// トグルボタンに変更があった時の処理
+				actionToggleButton(isChecked);
+			}
+		});
+	}
+	
+	// トグルボタンに変更があった時の処理
+	// state: trueならON
+	private void actionToggleButton(boolean state) {
+		
+		if(state) {
+			Toast.makeText(this, "on", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "off", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
